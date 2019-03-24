76fc9a745bb5dfcfd088e5b8518607950adbac84
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2019-03-24 19:32:26 -0400

    Arreglando errores de configuracion

--- core/src/com/asteroids/game/Game.java
+++ core/src/com/asteroids/game/Game.java
@@ -3,13 +3,14 @@ package com.asteroids.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	OrthographicCamera camera;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
